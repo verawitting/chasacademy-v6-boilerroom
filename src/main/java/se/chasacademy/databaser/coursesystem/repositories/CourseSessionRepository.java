80467d9ffd89c1969228333d4330a716670c4ba7
Oliver Öhrström <xzereha@live.se>
2025-12-16 09:38:22 +0100

    Added filtered find all to session

--- src/main/java/se/chasacademy/databaser/coursesystem/repositories/CourseSessionRepository.java
+++ src/main/java/se/chasacademy/databaser/coursesystem/repositories/CourseSessionRepository.java
@@ -1,5 +1,7 @@
 package se.chasacademy.databaser.coursesystem.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import se.chasacademy.databaser.coursesystem.models.CourseSession;
 
 @Repository
 public interface CourseSessionRepository extends JpaRepository<CourseSession, Long> {
+    List<CourseSession> findByCourseId(long courseId);
 
+    List<CourseSession> findByRoomId(long roomId);
 }
