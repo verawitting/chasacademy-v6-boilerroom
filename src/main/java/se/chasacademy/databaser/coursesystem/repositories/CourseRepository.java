75b6f73715307ddc36aa937800542168d6c07d1f
Millaray Soto Chavez <xmsc95@hotmail.se>
2025-12-16 10:34:25 +0100

    fixed findAllByTeacherId query

--- src/main/java/se/chasacademy/databaser/coursesystem/repositories/CourseRepository.java
+++ src/main/java/se/chasacademy/databaser/coursesystem/repositories/CourseRepository.java
@@ -2,6 +2,7 @@ package se.chasacademy.databaser.coursesystem.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import se.chasacademy.databaser.coursesystem.models.Course;
 
 import java.util.List;
@@ -12,10 +13,12 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     Optional<Course> findByTitle(String title);
 
     @Query("""
-            select c from Course c
-            join fetch c.teacher
-            """)
-    List<Course> findAllByTeacherId();
+        select c
+        from Course c
+        join fetch c.teacher t
+        where t.id = :teacherId
+    """)
+    List<Course> findAllByTeacherId(@Param("teacherId") Long teacherId);
 
     @Query("""
         select case
