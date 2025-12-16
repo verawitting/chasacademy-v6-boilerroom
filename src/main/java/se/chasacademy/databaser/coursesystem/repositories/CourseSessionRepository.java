73fdf2f44235055ef2b2574329b88f7fc3f8a0f4
Oliver Öhrström <xzereha@live.se>
2025-12-16 09:11:08 +0100

    Session Repository

---
+++ src/main/java/se/chasacademy/databaser/coursesystem/repositories/CourseSessionRepository.java
@@ -0,0 +1,11 @@
+package se.chasacademy.databaser.coursesystem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import se.chasacademy.databaser.coursesystem.models.CourseSession;
+
+@Repository
+public interface CourseSessionRepository extends JpaRepository<CourseSession, Long> {
+
+}
