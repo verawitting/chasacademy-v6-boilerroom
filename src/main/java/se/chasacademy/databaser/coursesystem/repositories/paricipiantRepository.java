866d47f8578c5907decf48231dfb34390c4a3c72
Mariozalac716 <zalac.mario1@gmail.com>
2025-12-16 09:05:18 +0100

    init participant

--- src/main/java/se/chasacademy/databaser/coursesystem/repositories/paricipiantRepository.java
+++ src/main/java/se/chasacademy/databaser/coursesystem/repositories/paricipiantRepository.java
@@ -3,5 +3,5 @@ package se.chasacademy.databaser.coursesystem.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.chasacademy.databaser.coursesystem.models.participant;
 
-public interface paricipiant extends JpaRepository<participant, Long> {
+public interface paricipiantRepository extends JpaRepository<participant, Long> {
 }
